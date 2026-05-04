3ef4153f13b19dd19f2ee51c45908e1f89aa6c63
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-05-05 01:26:08 +0400

    fix:autofocus directive

--- src/app/directives/autofocus.ts
+++ src/app/directives/autofocus.ts
@@ -1,9 +1,9 @@
-import { Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]',standalone: true
 })
-export class Autofocus {
+export class Autofocus implements AfterViewInit {
 
   constructor(private el:ElementRef) { }
 
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FilmService } from '../../services/film.service';
 import { FilmCard } from '../../components/film-card/film-card';
+import { Autofocus } from '../../directives/autofocus';
+
 @Component({
   selector: 'app-home',
-  imports: [FilmCard],
+  imports: [FilmCard,Autofocus],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
