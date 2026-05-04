2e2f720eded97d6c759b01fecabdd9f4efb9eaf1
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-05-04 23:53:01 +0400

    add:film service

---
+++ src/app/services/film.service.ts
@@ -0,0 +1,39 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { Film } from '../models/film.model';
+import filmsData from '../mock-data/films.json';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilmService {
+  private films = signal<Film[]>(filmsData as Film[]);
+  private searchQuery = signal<string>('');
+
+  filteredFilsm = computed(() => {
+    const query = this.searchQuery().toLowerCase().trim();
+
+    if (!query) {
+      return this.films();
+    }
+
+    return this.films().filter((film) => film.title.toLowerCase().includes(query));
+  });
+
+  favoriteFilms = computed(() => this.films().filter((film) => film.isFavorite));
+
+  setSearchQuery(query: string) {
+    this.searchQuery.set(query);
+  }
+
+  getFilmById(id: number): Film | undefined {
+    return this.films().find((film) => film.id === id);
+  }
+
+  toggleFavorite(id: number) {
+    this.films.update((films) =>
+      films.map((film) =>
+        film.id === id ? { ...film, isFavorite: !film.isFavorite } : film
+      )
+    );
+  }
+}
